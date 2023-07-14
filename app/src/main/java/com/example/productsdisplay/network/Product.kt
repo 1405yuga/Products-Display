6183ee68fc3ff477f972edd86d0109801d813bf5
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-14 10:38:20 +0530

    created data class

---
+++ app/src/main/java/com/example/productsdisplay/network/Product.kt
@@ -0,0 +1,10 @@
+package com.example.productsdisplay.network
+
+data class Product(
+    val name : String,
+    val price : String,
+    val brand : String,
+    val category: String,
+    val userType : String
+) {
+}
