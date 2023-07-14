cc1be82ccc5878c3792236e66bf2bdc8e859d4dd
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-14 10:48:10 +0530

    created moshi builder

--- app/src/main/java/com/example/productsdisplay/network/ProductsApiService.kt
+++ app/src/main/java/com/example/productsdisplay/network/ProductsApiService.kt
@@ -1,4 +1,16 @@
 package com.example.productsdisplay.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+
+//base url
+private const val BASE_URL ="https://automationexercise.com/api/"
+
+//moshi builder - to parse json
+private val moshiBuilder = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 interface ProductsApiService {
 }
