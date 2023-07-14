dda3416ee2bb335ff6c23d554426186257152947
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-14 10:50:58 +0530

    created retrofit builder

--- app/src/main/java/com/example/productsdisplay/network/ProductsApiService.kt
+++ app/src/main/java/com/example/productsdisplay/network/ProductsApiService.kt
@@ -2,6 +2,8 @@ package com.example.productsdisplay.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 
 //base url
@@ -10,6 +12,12 @@ private const val BASE_URL ="https://automationexercise.com/api/"
 //moshi builder - to parse json
 private val moshiBuilder = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
+    .build()
+
+//retrofit builder - to connect api
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshiBuilder))
+    .baseUrl(BASE_URL)
     .build()
 
 interface ProductsApiService {
