657d70dcf074b41084c46270c0321258ef177e18
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-14 10:54:02 +0530

    created suspend functions to get products list

--- app/src/main/java/com/example/productsdisplay/network/ProductsApiService.kt
+++ app/src/main/java/com/example/productsdisplay/network/ProductsApiService.kt
@@ -4,6 +4,7 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
 
 
 //base url
@@ -21,4 +22,8 @@ private val retrofit = Retrofit.Builder()
     .build()
 
 interface ProductsApiService {
+
+    //suspend function to get data
+    @GET("productsList")
+    suspend fun getProductsList() : List<Product>
 }
