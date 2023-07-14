520f7a0b82e9729122df9bdff9a32a3a77212cc8
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-14 11:30:27 +0530

    testing in log

--- app/src/main/java/com/example/productsdisplay/MainActivity.kt
+++ app/src/main/java/com/example/productsdisplay/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.productsdisplay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel = ProductViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG,"List "+ viewModel.getProducts())
     }
 }
