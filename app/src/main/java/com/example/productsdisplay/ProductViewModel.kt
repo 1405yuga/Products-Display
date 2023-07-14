6fdd3d1a6c34e2823b9db2e458538a613cfc5ca6
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-14 11:19:13 +0530

    created function to get product list in viewmodel

--- app/src/main/java/com/example/productsdisplay/ProductViewModel.kt
+++ app/src/main/java/com/example/productsdisplay/ProductViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.productsdisplay
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.productsdisplay.network.Product
-
+import com.example.productsdisplay.network.ProductsApi
+import kotlinx.coroutines.launch
+const val TAG = "ProductViewModel"
 
 class ProductViewModel : ViewModel(){
 
@@ -13,5 +16,14 @@ class ProductViewModel : ViewModel(){
     val products : LiveData<List<Product>> = _products
 
     fun getProducts(){
+        viewModelScope.launch {
+            try{
+               _products.value = ProductsApi.retrofitService.getProductsList()
+                Log.d(TAG,"getProductsList() ")
+            }catch (e : Exception){
+                _products.value = listOf()
+                Log.d(TAG,"exception : "+e.message)
+            }
+        }
     }
 }
